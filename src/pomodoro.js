11b636dae7796bf19cfe58ec2b6226f7559d51a1
Christopher Jee <me@chrisj.ee>
2019-01-22 18:57:00 +0000

    Add blinking for pause

--- src/pomodoro.js
+++ src/pomodoro.js
@@ -4,9 +4,9 @@ import History from './history.js';
 import Controls from './controls.js';
 
 const NUM_MINUTES_FOCUS = 25;
-const NUM_SECONDS_FOCUS = 5; //NUM_MINUTES_FOCUS*60;
-const NUM_SECONDS_BREAK = 1; //5*60;
-const NUM_SECONDS_LONGBREAK = 10; //30*60;
+const NUM_SECONDS_FOCUS = NUM_MINUTES_FOCUS*60;
+const NUM_SECONDS_BREAK = 5*60;
+const NUM_SECONDS_LONGBREAK = 30*60;
 const INTERVAL_FOCUS = "Focus";
 const INTERVAL_BREAK = "Break";
 const INTERVAL_LONGBREAK = "Long Break";
@@ -26,13 +26,19 @@ class Pomodoro extends React.Component {
             seconds: minSec.seconds,
             isFocus: true,
             isPlay: false,
+            isBlink: true,
             history: [],
         };
     }
 
     render() {
         return (
-            <div>
+            <div style={{
+                backgroundColor: (
+                    this.state.isBlink
+                        ? (this.state.isFocus ? "#8CC152" : "#E9573F" )
+                        : "#FFFFFF")
+            }}>
                 <h1>Pomodoro</h1>
                 <Timer
                     minutes={this.state.minutes}
@@ -53,9 +59,11 @@ class Pomodoro extends React.Component {
     handlePlayPause() {
         console.log(`Play / pause clicked, isPlay = ${this.state.isPlay}`);
         if (this.state.isPlay === true) {
-            clearInterval(this.intervalHandle);
             this.appendHistory(INTERVAL_PAUSE);
+            clearInterval(this.intervalHandle);
+            this.intervalHandle = setInterval(() => this.handleTick(), 250);
         } else {
+            clearInterval(this.intervalHandle);
             this.intervalHandle = setInterval(() => this.handleTick(), 1000);
 
             // If starting an interval, log in history
@@ -76,39 +84,45 @@ class Pomodoro extends React.Component {
 
     handleTick() {
         console.log(`Tick`);
-        this.numSeconds--;
-        let minSec = this.getMinSec();
-        let isFocus = this.state.isFocus;
-        if (minSec.minutes === 0 && minSec.seconds === 0) {
-            if (isFocus === true) {
-                isFocus = false;
-                this.numPomodoros++;
-                if (this.numPomodoros === 4) {
-                    this.numPomodoros = 0;
-                    this.numSeconds = NUM_SECONDS_LONGBREAK;
-                    this.currentInterval = INTERVAL_LONGBREAK;
+        if (this.state.isPlay === true) {
+            this.numSeconds--;
+            let minSec = this.getMinSec();
+            let isFocus = this.state.isFocus;
+            if (minSec.minutes === 0 && minSec.seconds === 0) {
+                if (isFocus === true) {
+                    isFocus = false;
+                    this.numPomodoros++;
+                    if (this.numPomodoros === 4) {
+                        this.numPomodoros = 0;
+                        this.numSeconds = NUM_SECONDS_LONGBREAK;
+                        this.currentInterval = INTERVAL_LONGBREAK;
+                    } else {
+                        this.numSeconds = NUM_SECONDS_BREAK;
+                        this.currentInterval = INTERVAL_BREAK;
+                    }
                 } else {
-                    this.numSeconds = NUM_SECONDS_BREAK;
-                    this.currentInterval = INTERVAL_BREAK;
+                    isFocus = true;
+                    this.numSeconds = NUM_SECONDS_FOCUS;
+                    this.currentInterval = INTERVAL_FOCUS;
                 }
-            } else {
-                isFocus = true;
-                this.numSeconds = NUM_SECONDS_FOCUS;
-                this.currentInterval = INTERVAL_FOCUS;
+
+                // Refresh minSec calculation after having reset the countdown appropriately.
+                minSec = this.getMinSec();
+
+                // Pause the timer, so the user can get ready for the next interval
+                this.handlePlayPause();
             }
 
-            // Refresh minSec calculation after having reset the countdown appropriately.
-            minSec = this.getMinSec();
-
-            // Pause the timer, so the user can get ready for the next interval
-            this.handlePlayPause();
+            this.setState({
+                isFocus: isFocus,
+                minutes: minSec.minutes,
+                seconds: minSec.seconds,
+            });
+        } else {
+            this.setState({
+                isBlink: !this.state.isBlink,
+            });
         }
-
-        this.setState({
-            isFocus: isFocus,
-            minutes: minSec.minutes,
-            seconds: minSec.seconds,
-        });
     }
 
     getMinSec() {
